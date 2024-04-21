55a1c7ef494b8942268c0dc1d4b21b9929b5a499
Mario <mperdiguerobarrera@safareyes.es>
2024-04-21 23:30:53 +0200

    ✨ Create equals and hashCode methods

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Person {
 
     private String name;
@@ -39,6 +41,18 @@ public class Person {
     @Override
     public String toString() {
         return name + " " + lastName + " (" + age + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(lastName, person.lastName) && Objects.equals(age, person.age);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, lastName, age);
     }
 
 }
