e7a6e5441c30635f03174125b378e65320bf6b86
Mario <marioperdiguero37@gmail.com>
2024-04-25 12:03:58 +0200

    🔥 Remove unused import

--- src/main/java/controller/ModalController.java
+++ src/main/java/controller/ModalController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
